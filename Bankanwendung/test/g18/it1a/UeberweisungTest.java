3c0221b0e35405c935f0423b56be1301faa50d23
it1-kudlmi <it1-kudlmi@R053-19.g18.de>
2013-04-05 11:32:36 +0200

    test erstellt

--- Bankanwendung/test/g18/it1a/UeberweisungTest.java
+++ Bankanwendung/test/g18/it1a/UeberweisungTest.java
@@ -1,14 +1,21 @@
 package g18.it1a;
 
-import static org.junit.Assert.*;
+import junit.framework.Assert;
+import g18.it1a.Konto.KontoTyp;
 
 import org.junit.Test;
 
 public class UeberweisungTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void KannÜberweiungDurchführen() {
+		Konto quelle = new Konto(1, 100, KontoTyp.GIROKONTO);
+		Konto ziel = new Konto(1, 50, KontoTyp.GIROKONTO);
+		Ueberweisung ueberweisung = new Ueberweisung(quelle, ziel, 50, null);
+		ueberweisung.durchfuehrenUeberweisung();
+		int actual = (int) ziel.getKontostand();
+		int expected = 100;
+		
+		Assert.assertEquals(expected, actual);
 	}
-
 }
