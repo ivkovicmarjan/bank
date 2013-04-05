d3fb37a488d0f2af34dfbbf9bcb966b92fbcdd04
it1-kudlmi <it1-kudlmi@R053-19.g18.de>
2013-04-05 10:40:55 +0200

    konto und ueberweisung implementiert

--- Bankanwendung/src/g18/it1a/Kontobewegung.java
+++ Bankanwendung/src/g18/it1a/Kontobewegung.java
@@ -1,5 +1,40 @@
 package g18.it1a;
 
-public class Kontobewegung {
+import java.util.Date;
 
+public class Kontobewegung 
+{
+	private Date datum;
+	private long betrag;
+	private String bemerkung;
+	
+	public Date getDatum()
+	{
+		return this.datum;
+	}
+	
+	public void setDatum(Date datum) 
+	{
+		this.datum = datum;
+	}
+	
+	public long getBetrag()
+	{
+		return betrag;
+	}
+	
+	public void setBetrag(long betrag)
+	{
+		this.betrag = betrag;
+	}
+	
+	public String getBemerkung() 
+	{
+		return this.bemerkung;
+	}
+	
+	public void setBemerkung(String bemerkung)
+	{
+		this.bemerkung = bemerkung;
+	}
 }
--- Bankanwendung/src/g18/it1a/Ueberweisung.java
+++ Bankanwendung/src/g18/it1a/Ueberweisung.java
@@ -1,5 +1,48 @@
 package g18.it1a;
 
-public class Ueberweisung {
+import java.util.Date;
 
+public class Ueberweisung 
+{
+	private Date datum;
+	private long betrag;
+	private Konto quelle;
+	private Konto ziel;
+	
+	public Date getDatum() {
+		return this.datum;
+	}
+	
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+	
+	public long getBetrag() {
+		return this.betrag;
+	}
+	
+	public void setBetrag(long betrag) {
+		this.betrag = betrag;
+	}
+	
+	public Konto getQuelle() {
+		return this.quelle;
+	}
+	
+	public void setQuelle(Konto quelle) {
+		this.quelle = quelle;
+	}
+	
+	public Konto getZiel() {
+		return this.ziel;
+	}
+	
+	public void setZiel(Konto ziel) {
+		this.ziel = ziel;
+	}
+
+	public void durchfuehrenUeberweisung()
+	{
+		
+	}
 }
