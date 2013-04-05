005c4204ecaa49b02ebaa91562320f8f96ce4efc
it1-thomle <it1-thomle@R053-20.g18.de>
2013-04-05 12:33:26 +0200

    Kunde implementiert

--- Bankanwendung/test/g18/it1a/KundeTest.java
+++ Bankanwendung/test/g18/it1a/KundeTest.java
@@ -1,6 +1,9 @@
 package g18.it1a;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import junit.framework.Assert;
 import g18.it1a.Konto.KontoTyp;
 
@@ -8,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class KundeTest {
-	
+
 	private Kunde kunde;
 
 	@Before
@@ -18,20 +21,28 @@ public class KundeTest {
 
 	@Test
 	public void testKunde() {
-		fail("Not yet implemented");
+		Kunde kunde2 = new Kunde("John Doe", 2);
+		assertEquals("John Doe", kunde2.getName());
+		assertEquals(2, kunde2.getKundenNummer());
+		assertEquals(new ArrayList<Konto>(), kunde2.getKonten());
 	}
 
 	@Test
 	public void testAnzeigenKontostandsUebersicht() {
-		assertEquals("keine Konten vorhanden", kunde.anzeigenKontostandsUebersicht() );
+		assertEquals("keine Konten vorhanden",
+				kunde.anzeigenKontostandsUebersicht());
 		kunde.anlegenKonto(KontoTyp.GIROKONTO);
 		kunde.anlegenKonto(KontoTyp.SPARKONTO);
-		assertEquals("keine Konten vorhanden", kunde.anzeigenKontostandsUebersicht() );
+		String ausgabe = "Übersicht der Konten von Max Mustermann:\n"
+				+ "Kontonummer: 101001, Kontostand: 0.0, Kontotyp: GIROKONTO\n\n"
+				+ "Kontonummer: 100002, Kontostand: 0.0, Kontotyp: SPARKONTO\n\n";
+
+		assertEquals(ausgabe, kunde.anzeigenKontostandsUebersicht());
 	}
 
 	@Test
 	public void testAnlegenKonto() {
-		
+
 	}
 
 	@Test
@@ -39,15 +50,29 @@ public class KundeTest {
 		Konto girokonto = kunde.anlegenKonto(KontoTyp.GIROKONTO);
 		girokonto.einzahlen(100);
 		kunde.auszahlenBetrag(girokonto, 100);
-		Assert.assertEquals(0, girokonto.getKontostand());
-				
+		Assert.assertEquals(0.0, girokonto.getKontostand());
+
+		kunde.auszahlenBetrag(girokonto, 100);
+		Assert.assertEquals(0.0, girokonto.getKontostand());
+
 		Konto sparkonto = kunde.anlegenKonto(KontoTyp.SPARKONTO);
+		sparkonto.einzahlen(200);
 		kunde.auszahlenBetrag(sparkonto, 75);
+		Assert.assertEquals(125.0, sparkonto.getKontostand());
+		kunde.auszahlenBetrag(sparkonto, 475);
+		Assert.assertEquals(125.0, sparkonto.getKontostand());
+
 	}
 
 	@Test
 	public void testEinzahlenBetrag() {
-		fail("Not yet implemented");
+		Konto girokonto = kunde.anlegenKonto(KontoTyp.GIROKONTO);
+		kunde.einzahlenBetrag(girokonto, 100.0);
+		Assert.assertEquals(100.0, girokonto.getKontostand());
+
+		Konto sparkonto = kunde.anlegenKonto(KontoTyp.SPARKONTO);
+		kunde.einzahlenBetrag(sparkonto, 100.0);
+		Assert.assertEquals(100.0, sparkonto.getKontostand());
 	}
 
 }
