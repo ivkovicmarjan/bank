51ffe77f45ef5ca813cc33277e2434e85986f6d9
it1-kudlmi <it1-kudlmi@R053-19.g18.de>
2013-04-05 10:55:45 +0200

    created tests

---
+++ Bankanwendung/test/g18/it1a/KontobewegungTest.java
@@ -0,0 +1,14 @@
+package g18.it1a;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KontobewegungTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Bankanwendung/test/g18/it1a/UeberweisungTest.java
@@ -0,0 +1,14 @@
+package g18.it1a;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UeberweisungTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
