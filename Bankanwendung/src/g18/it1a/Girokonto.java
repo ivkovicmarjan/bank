1ae69230b4f5f1c9fe158d5ba3fec8d694d8893b
it1-kudlmi <it1-kudlmi@R053-19.g18.de>
2013-04-05 10:46:14 +0200

    implemented stuff

--- Bankanwendung/src/g18/it1a/Girokonto.java
+++ Bankanwendung/src/g18/it1a/Girokonto.java
@@ -1,5 +1,18 @@
 package g18.it1a;
 
-public class Girokonto {
+public class Girokonto 
+{
+	private int dispo;
+	
+	public int getDispo() {
+		return this.dispo;
+	}
 
+	public void setDispo(int dispo) {
+		this.dispo = dispo;
+	}
+	
+	public void operation(){
+
+	}
 }
--- Bankanwendung/src/g18/it1a/Sparkonto.java
+++ Bankanwendung/src/g18/it1a/Sparkonto.java
@@ -1,5 +1,18 @@
 package g18.it1a;
 
-public class Sparkonto {
+public class Sparkonto
+{
+	private int zinssatz;
+	
+	public int getZinssatz() {
+		return this.zinssatz;
+	}
 
+	public void setZinssatz(int zinssatz) {
+		this.zinssatz = zinssatz;
+	}
+	
+	public int ausszahlen(){
+		return 0;
+	}
 }
