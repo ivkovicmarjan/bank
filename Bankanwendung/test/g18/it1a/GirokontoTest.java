b58bcad3d877d7d67128a9270d082ec74018ec11
it1-thomle <it1-thomle@R053-20.g18.de>
2013-04-05 10:54:43 +0200

    junit eingebaut

---
+++ Bankanwendung/test/g18/it1a/GirokontoTest.java
@@ -0,0 +1,14 @@
+package g18.it1a;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GirokontoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
