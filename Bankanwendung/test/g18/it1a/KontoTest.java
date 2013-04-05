06ccafef380f9e443f6c3f6447d31d24b21a0ae6
it1-nießka <it1-nieka@R053-18.g18.de>
2013-04-05 11:20:50 +0200

    Konto

--- Bankanwendung/test/g18/it1a/KontoTest.java
+++ Bankanwendung/test/g18/it1a/KontoTest.java
@@ -2,13 +2,14 @@ package g18.it1a;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class KontoTest {
 
-	@Test
+	@Before
 	public void test() {
-		fail("Not yet implemented");
+		Konto konto = new Konto(12345678, 0.00, "GIROKONTO");
 	}
 
 }
