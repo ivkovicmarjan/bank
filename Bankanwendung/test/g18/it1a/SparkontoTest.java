3979f684b30e4bd4a06f7bf2bf54e69398a83f89
it1-nießka <it1-nieka@R053-18.g18.de>
2013-04-11 12:46:12 +0200

    SparkontoTest implementiert

--- Bankanwendung/test/g18/it1a/SparkontoTest.java
+++ Bankanwendung/test/g18/it1a/SparkontoTest.java
@@ -1,14 +1,30 @@
 package g18.it1a;
 
-import static org.junit.Assert.*;
+import g18.it1a.Konto.KontoTyp;
+import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SparkontoTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private int kontonummer;
+	private KontoTyp kontotyp;
+	private Sparkonto sparkonto;
+	
+	@Before
+	public void testInstanziierung() {
+		kontonummer = 13145657;
+		kontotyp = KontoTyp.SPARKONTO;
+		sparkonto = new Sparkonto(kontonummer, kontotyp);
 	}
-
+	
+	@Test
+	public void testAuszahlen() {
+		sparkonto.setKontostand(100.0);
+		sparkonto.ausszahlen(110.0);
+		Assert.assertEquals(100.0, sparkonto.getKontostand());
+		sparkonto.auszahlen(90.0);
+		Assert.assertEquals(10.0, sparkonto.getKontostand());
+	}
 }
