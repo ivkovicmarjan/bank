04eed3bfabcc26c3fb11eede52fc0a5145bb1bfa
it1-thomle <it1-thomle@R053-20.g18.de>
2013-04-19 09:19:38 +0200

    crap

--- Bankanwendung/src/g18/it1a/controller/CtlBankView.java
+++ Bankanwendung/src/g18/it1a/controller/CtlBankView.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 
 import g18.it1a.model.Konto;
 import g18.it1a.model.Kunde;
