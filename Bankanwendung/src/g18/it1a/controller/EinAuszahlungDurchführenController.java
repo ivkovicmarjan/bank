4a2acc7c8d5dfaa87dc83c7520459dba4030329d
it1-nießka <it1-nieka@R053-18.g18.de>
2013-04-25 12:42:53 +0200

    Auszaghlen

--- Bankanwendung/src/g18/it1a/controller/EinAuszahlungDurchführenController.java
+++ Bankanwendung/src/g18/it1a/controller/EinAuszahlungDurchführenController.java
@@ -1,6 +1,7 @@
 package g18.it1a.controller;
 
 import g18.it1a.exceptions.AccountNotFoundException;
+import g18.it1a.exceptions.LiquidityException;
 import g18.it1a.model.Kunden;
 import g18.it1a.view.EinAuszahlungDurchführenDlg;
 
@@ -24,14 +25,52 @@ public class EinAuszahlungDurchführenController {
 
 		einAuszahlungDurchführenDlg.getAuszahlenButton().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				btAuszahlenActionPerformed(einAuszahlungDurchführenDlg.getBetragField().getText());
+				btAuszahlenActionPerformed(einAuszahlungDurchführenDlg.getBetragField().getText(), einAuszahlungDurchführenDlg.getKundennummerField().getText());
 			}
 		});
 	}
 
-	private void btAuszahlenActionPerformed(String text) {
+	private void btAuszahlenActionPerformed(String value, String kontoNummer) {
+		double betrag;
+		long kontonummer;
+
+		try {
+			kontonummer = Long.valueOf(kontoNummer);
+		} catch (NumberFormatException e) {
+			String result = JOptionPane.showInputDialog(einAuszahlungDurchführenDlg, "Bitte Zahl als Kontonummer eingeben.");
+			btAuszahlenActionPerformed(value, result);
+			return;
+		}
+		
+		try {
+			betrag = Double.valueOf(value);
+		} catch (NumberFormatException e) {
+			String result = JOptionPane.showInputDialog(einAuszahlungDurchführenDlg, "Bitte Zahl als Betrag eingeben.");
+			btAuszahlenActionPerformed(result, kontoNummer);
+			return;
+		}
+		
+		einAuszahlungDurchführenDlg.getKundennummerField().setText(kontoNummer);
+		einAuszahlungDurchführenDlg.getBetragField().setText(value);
+
+		
+		
+		try {
+			einAuszahlungDurchführenDlg.getAlterKontostandField().setText(""+ControllerUtils.getKonto(kontonummer).getKontostand());
+			Kunden.auszahlen(betrag, kontonummer);
+			einAuszahlungDurchführenDlg.getNeuerKontostandField().setText(""+ControllerUtils.getKonto(kontonummer).getKontostand());
+		} catch (AccountNotFoundException e) {
+			JOptionPane.showMessageDialog(einAuszahlungDurchführenDlg, "Konto ist nicht vorhanden!");
+			return;
+		} catch (LiquidityException e) {
+			JOptionPane.showMessageDialog(einAuszahlungDurchführenDlg, "Nicht genug Geld auf dem Konto!");
+			return;
+		}
+		
+		JOptionPane.showMessageDialog(einAuszahlungDurchführenDlg, "Auszahlung wurde durchgeführt!");
 		
 	}
+	
 
 	private void btEinzahlenActionPerformed(String value, String kontoNummer) {
 		double betrag;
